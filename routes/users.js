864f7664ca55d309d9a526dcf8d9ccfb626b280e
dalbamm <oscar.yoon13@gmail.com>
2021-01-25 09:30:40 +0000

    init2

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signin', function(req, res, next) {
+  res.send('signin');
+});
+
+router.get('/signup', function(req, res, next) {
+  res.send('signup');
+});
+
+router.get('/validate', function(req, res, next) {
+  res.send('validate');
 });
 
 module.exports = router;
