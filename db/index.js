17578532f0c4e58871761334b1f6081c1155f5ca
dalbamm <oscar.yoon13@gmail.com>
2021-01-25 09:46:01 +0000

    add entities and schemes

---
+++ db/index.js
@@ -0,0 +1,16 @@
+var sctKey = require('jsonwebtoken');
+//var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+
+//db connection
+
+//findByInfo(id, pw)
+
+//__list() (find all user)
+
+//insertByInfo(id, pw)
+
+//findBySecretKey(sctKey)
+
+//*encryptSecretKey()
+
+module.exports = sctKey;
